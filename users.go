8dc8c00ec935be2e4c516cb38558d0997d2f24c5
Oleksiy Hryshchenko <greeschenko@gmail.com>
2018-08-04 02:20:39 +0300

    init

---
+++ users.go
@@ -0,0 +1,403 @@
+package users
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"go-rest-framework/modules/gorest"
+	"go-rest-framework/modules/res"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var App gorest.App
+
+type Users []User
+
+type User struct {
+	gorm.Model
+	Email       string `gorm:"unique;not null" valid:"email,required,unique~Email not unique"`
+	Password    string `valid:"ascii,required"`
+	Role        string
+	Token       string
+	Salt        string `json:"-"`
+	CheckToken  string `json:"-"`
+	CallBackUrl string `gorm:"-"`
+}
+
+type UserUpdate struct {
+	ID       uint
+	Email    string `valid:"email"`
+	Password string `valid:"ascii"`
+	Role     string
+}
+
+type Login struct {
+	Email    string `valid:"email,required"`
+	Password string `valid:"ascii,required"`
+}
+
+type Confirm struct {
+	CheckToken string `valid:"required"`
+}
+
+type ResetRequest struct {
+	Email       string `valid:"email,required"`
+	CallBackUrl string
+}
+
+type Reset struct {
+	CheckToken string `valid:"required"`
+	Newpass    string `valid:"required"`
+	NewpassRe  string `valid:"required"`
+}
+
+func Configure(a gorest.App) {
+	App = a
+
+	App.DB.AutoMigrate(&User{})
+
+	createAdmin()
+
+	//public actions
+	App.R.HandleFunc("/api/users/register", srvRegister).Methods("POST")
+	App.R.HandleFunc("/api/users/login", srvLogin).Methods("POST")
+	App.R.HandleFunc("/api/users/confirm", srvConfirm).Methods("POST")
+	App.R.HandleFunc("/api/users/resetrequest", srvResetrequest).Methods("POST")
+	App.R.HandleFunc("/api/users/reset", srvReset).Methods("POST")
+
+	//protect actions
+	App.R.HandleFunc("/api/users/get-all", App.Protect(srvGetAll, []string{"admin"})).Methods("GET")
+	App.R.HandleFunc("/api/users/get-one/{id}", App.Protect(srvGetOne, []string{"admin"})).Methods("GET")
+	App.R.HandleFunc("/api/users/create", App.Protect(srvCreate, []string{"admin"})).Methods("POST")
+	App.R.HandleFunc("/api/users/update", App.Protect(srvUpdate, []string{"admin"})).Methods("POST")
+	App.R.HandleFunc("/api/users/delete", App.Protect(srvDelete, []string{"admin"})).Methods("POST")
+}
+
+func srvGetOne(w http.ResponseWriter, r *http.Request) {
+	var (
+		user User
+		rsp  = res.Response{Data: &user}
+	)
+
+	vars := mux.Vars(r)
+	App.DB.First(&user, vars["id"])
+
+	if user.ID == 0 {
+		rsp.Errors.Add("ID", "User not found")
+	} else {
+		rsp.Data = &user
+	}
+
+	w.Write(rsp.Make())
+}
+
+func srvGetAll(w http.ResponseWriter, r *http.Request) {
+	var (
+		users Users
+		rsp   = res.Response{Data: &users}
+	)
+
+	App.DB.Find(&users)
+
+	rsp.Data = &users
+
+	w.Write(rsp.Make())
+}
+
+func srvCreate(w http.ResponseWriter, r *http.Request) {
+	var (
+		user User
+		rsp  = res.Response{Data: &user}
+	)
+
+	govalidator.TagMap["unique"] = govalidator.Validator(func(str string) bool {
+		App.DB.Where("email = ?", str).First(&user)
+		if user.ID != 0 {
+			return false
+		}
+		return true
+	})
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			curtime := fmt.Sprintf("%x", time.Now())
+			passsalt := App.ToSum256(curtime)
+			passhash := App.ToSum256(user.Password + passsalt)
+			user.Password = passhash
+			user.Salt = passsalt
+			App.DB.Create(&user)
+		}
+	}
+
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvUpdate(w http.ResponseWriter, r *http.Request) {
+	var (
+		data UserUpdate
+		user User
+		rsp  = res.Response{Data: &data}
+	)
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			if data.ID == 0 {
+				rsp.Errors.Add("ID", "ID not set")
+			} else {
+				App.DB.First(&user, data.ID)
+				if user.ID == 0 {
+					rsp.Errors.Add("ID", "User not found")
+				} else {
+					App.DB.Model(&user).Updates(data)
+				}
+			}
+		}
+	}
+
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvDelete(w http.ResponseWriter, r *http.Request) {
+	var (
+		data User
+		user User
+		rsp  = res.Response{Data: &data}
+	)
+
+	if rsp.IsJsonParseDone(r.Body) {
+		App.DB.First(&user, data.ID)
+		if user.ID == 0 {
+			rsp.Errors.Add("ID", "User not found")
+		} else {
+			if App.IsTest {
+				App.DB.Unscoped().Delete(&user)
+			} else {
+				App.DB.Delete(&user)
+			}
+		}
+	}
+
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvLogin(w http.ResponseWriter, r *http.Request) {
+	var (
+		data Login
+		user User
+		rsp  = res.Response{Data: &data}
+	)
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			App.DB.Where("email = ?", data.Email).First(&user)
+			if user.ID == 0 || user.Password != App.ToSum256(data.Password+user.Salt) {
+				rsp.Errors.Add("Email", "User not found or wrong password")
+			} else if user.Role == "" || user.Role == "candidate" {
+				rsp.Errors.Add("Email", "You have not verified your email address")
+			} else {
+				token, err := App.GenToken(&user.Email, &user.Role)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					rsp.Errors.Add("Email", "Error generating JWT token: "+err.Error())
+				} else {
+					w.Header().Set("Authorization", "Bearer "+token)
+					w.WriteHeader(http.StatusOK)
+					user.Token = token
+				}
+			}
+		}
+	}
+
+	user.Password = ""
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvRegister(w http.ResponseWriter, r *http.Request) {
+	var (
+		user User
+		rsp  = res.Response{Data: &user}
+	)
+
+	govalidator.TagMap["unique"] = govalidator.Validator(func(str string) bool {
+		App.DB.Where("email = ?", str).First(&user)
+		if user.ID != 0 {
+			return false
+		}
+		return true
+	})
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			var checktoken string
+			curtime := fmt.Sprintf("%x", time.Now())
+			passsalt := App.ToSum256(curtime)
+			passhash := App.ToSum256(user.Password + passsalt)
+			if App.IsTest {
+				checktoken = "testchecktoken"
+			} else {
+				checktoken = App.ToSum256(fmt.Sprintf("%s.%x", user.Email, time.Now()))
+			}
+			user.Password = passhash
+			user.Salt = passsalt
+			user.Role = "candidate"
+			user.CheckToken = checktoken
+			log.Println(checktoken)
+			App.DB.Create(&user)
+			App.Mail.Send(
+				user.Email,
+				"Registration confirm",
+				"To confirm the registration, go to the link "+user.CallBackUrl+"?token="+checktoken,
+			)
+		}
+	}
+
+	user.Password = ""
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvConfirm(w http.ResponseWriter, r *http.Request) {
+	var (
+		data Confirm
+		user User
+		rsp  = res.Response{Data: &data}
+	)
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			App.DB.Where("check_token = ?", data.CheckToken).First(&user)
+			if user.ID == 0 {
+				rsp.Errors.Add("CheckToken", "User not found")
+			} else if user.Role != "" && user.Role != "candidate" {
+				rsp.Errors.Add("CheckToken", "You have already verified your email")
+			} else {
+				res := App.DB.Model(&user).Updates(map[string]interface{}{"role": "user", "check_token": ""})
+				if res.Error != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					rsp.Errors.Add("CheckToken", "Data saving error")
+					log.Println("Data saving error: " + res.Error.Error())
+				}
+			}
+		}
+	}
+
+	user.Password = ""
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvResetrequest(w http.ResponseWriter, r *http.Request) {
+	var (
+		data ResetRequest
+		user User
+		rsp  = res.Response{Data: &data}
+	)
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			var checktoken string
+			App.DB.Where("email = ?", data.Email).First(&user)
+			if user.ID == 0 {
+				rsp.Errors.Add("Email", "User not found")
+			} else if user.Role == "" || user.Role == "candidate" {
+				rsp.Errors.Add("Email", "You have not confirmed your email")
+			} else {
+				if App.IsTest {
+					checktoken = "testchecktoken"
+				} else {
+					checktoken = App.ToSum256(fmt.Sprintf("%s.%x", user.Email, time.Now()))
+				}
+				res := App.DB.Model(&user).Update("checktoken", checktoken)
+				if res.Error != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					rsp.Errors.Add("Email", "Data saving error")
+					log.Println("Data saving error: " + res.Error.Error())
+				} else {
+					App.Mail.Send(
+						user.Email,
+						"Password reset request",
+						"To reset your password, go to the link "+data.CallBackUrl+"?token="+checktoken,
+					)
+					log.Println(checktoken)
+				}
+			}
+		}
+	}
+
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func srvReset(w http.ResponseWriter, r *http.Request) {
+	var (
+		data Reset
+		user User
+		rsp  = res.Response{Data: &data}
+	)
+
+	if rsp.IsJsonParseDone(r.Body) {
+		if rsp.IsValidate() {
+			App.DB.Where("check_token = ?", data.CheckToken).First(&user)
+			if user.ID == 0 {
+				rsp.Errors.Add("Email", "User not found")
+			} else if user.Role == "" || user.Role == "candidate" {
+				rsp.Errors.Add("Email", "You have already verified your email")
+			} else if &data.Newpass != &data.NewpassRe {
+				rsp.Errors.Add("Newpass", "New password and new password repeat must be equal")
+			} else {
+				passhash := App.ToSum256(data.Newpass + user.Salt)
+				App.DB.Model(&user).Updates(map[string]interface{}{"password": passhash, "check_token": ""})
+			}
+		}
+	}
+
+	rsp.Data = &user
+
+	w.Write(rsp.Make())
+}
+
+func createAdmin() {
+	var (
+		user User
+	)
+
+	user.Email = "admin@admin.a"
+
+	App.DB.Where("email = ?", user.Email).First(&user)
+	if user.ID == 0 {
+		curtime := fmt.Sprintf("%x", time.Now())
+		passsalt := App.ToSum256(curtime)
+
+		if App.IsTest {
+			user.Password = "adminpass"
+		} else {
+			user.Password = curtime[1:16]
+		}
+
+		fmt.Printf("%s\n", user.Password)
+
+		passhash := App.ToSum256(user.Password + passsalt)
+
+		user.Password = passhash
+		user.Salt = passsalt
+		user.Role = "admin"
+		App.DB.Create(&user)
+	}
+}
---
+++ users_test.go
@@ -0,0 +1,474 @@
+package users_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-rest-framework/modules/res"
+	"go-rest-framework/modules/users"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+var Uid uint
+var Uidnew uint
+var AdminToken string
+var Murl = "http://gorest.ga/api/users/"
+
+type TestUsers struct {
+	Errors []res.ErrorMsg
+	Data   users.Users
+}
+
+type TestUser struct {
+	Errors []res.ErrorMsg
+	Data   users.User
+}
+
+func readUsersBody(r *http.Response, t *testing.T) TestUsers {
+	var u TestUsers
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal([]byte(body), &u)
+	return u
+}
+
+func doRequest(url, proto, userJson, token string) *http.Response {
+	reader := strings.NewReader(userJson)
+	request, err := http.NewRequest(proto, url, reader)
+	if token != "" {
+		request.Header.Set("Authorization", "Bearer "+token)
+	}
+	resp, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
+}
+
+func readUserBody(r *http.Response, t *testing.T) TestUser {
+	var u TestUser
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal([]byte(body), &u)
+	defer r.Body.Close()
+	return u
+}
+
+//"/api/users/register", srvRegister).Methods("POST")
+func TestRegister(t *testing.T) {
+
+	url := Murl + "register"
+	userJson := `{"Email":"sldfjsdlfeusdlfjsdlfj", "Password":"343223423423"}`
+
+	resp := doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("email type validation dont work")
+	}
+
+	userJson = `{"Email":"sdlfjldjflsdf@sldfjsdlf.eu"}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("require validation dont work")
+	}
+
+	userJson = `{"Email":"sldfjsdlf@eusdlfjsdlfj.com", "Password":"343223423423"}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	Uid = u.Data.ID
+
+	return
+}
+
+//"/api/users/confirm", srvConfirm).Methods("POST")
+func TestConfirm(t *testing.T) {
+
+	url := Murl + "confirm"
+	var userJson = `{"CheckToken":"wrongtoken"}`
+
+	resp := doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("token check fail")
+	}
+
+	userJson = `{}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("require validation dont work")
+	}
+
+	userJson = `{"CheckToken":"testchecktoken"}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	return
+}
+
+//"/api/users/login", srvLogin).Methods("POST")
+func TestLogin(t *testing.T) {
+
+	url := Murl + "login"
+	var userJson = `{"Email":"sdlf@eusdlfjsdlfj.com", "Password":"dddd343223423423"}`
+
+	resp := doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("password check fail")
+	}
+
+	userJson = `{"Email":"sldfjsdlf@eusdlfjsdlfj.com"}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("require validation dont work")
+	}
+
+	userJson = `{"Email":"sldfjsdlf@eusdlfjsdlfj.com", "Password":"343223423423"}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	return
+}
+
+//"/api/users/resetrequest", srvResetrequest).Methods("POST")
+func TestResetrequest(t *testing.T) {
+
+	url := Murl + "resetrequest"
+	var userJson = `{"Email":"sldfjsdlf@eusdlfjsdlfj.com", "CallBackUrl":"http://test.ttt"}`
+
+	resp := doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	return
+}
+
+//"/api/users/reset", srvReset).Methods("POST")
+func TestReset(t *testing.T) {
+
+	url := Murl + "reset"
+	var userJson = `{
+		"CheckToken":"testchecktoken",
+		"Newpass":"newpass",
+		"NewpassRe":"newpass1"
+	}`
+
+	resp := doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("check equal passwords fail")
+	}
+
+	userJson = `{
+		"CheckToken":"testchecktoken",
+		"Newpass":"newpass",
+		"NewpassRe":"newpass"
+	}`
+
+	resp = doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	return
+}
+
+func TestAdminLogin(t *testing.T) {
+
+	url := Murl + "login"
+	var userJson = `{"Email":"admin@admin.a", "Password":"adminpass"}`
+
+	resp := doRequest(url, "POST", userJson, "")
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	AdminToken = u.Data.Token
+
+	return
+}
+
+//"/api/users/create", App.Protect(srvCreate, []string{"admin"})).Methods("POST")
+func TestCreate(t *testing.T) {
+	url := Murl + "create"
+	userJson := `{
+		"Email":"newuser@user.u",
+		"Password":"newuserpass",
+		"Role":"user"
+	}`
+
+	resp := doRequest(url, "GET", userJson, AdminToken)
+
+	if resp.StatusCode == 200 {
+		t.Fatal("POST check dont work")
+	}
+
+	resp = doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	Uidnew = u.Data.ID
+
+	return
+}
+
+//"/api/users/get-all", App.Protect(srvGetAll, []string{"admin"})).Methods("GET")
+func TestGetAll(t *testing.T) {
+	// get count
+	url := Murl + "get-all"
+
+	resp := doRequest(url, "GET", "", "  ")
+
+	if resp.StatusCode == 200 {
+		t.Fatal("require validation dont work")
+	}
+
+	resp = doRequest(url, "GET", "", AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUsersBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	return
+}
+
+//"/api/users/get-one/{id}", App.Protect(srvGetOne, []string{"admin"})).Methods("GET")
+func TestGetOne(t *testing.T) {
+	url := Murl + "get-one/" + "0"
+	resp := doRequest(url, "GET", "", AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("element not found dont work")
+	}
+
+	url = fmt.Sprintf("%s%s%d", Murl, "get-one/", Uid)
+
+	resp = doRequest(url, "GET", "", AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	if u.Data.Email != "sldfjsdlf@eusdlfjsdlfj.com" {
+		t.Fatal("wrong email get")
+	}
+
+	return
+}
+
+//"/api/users/update", App.Protect(srvUpdate, []string{"admin"})).Methods("POST")
+func TestUpdate(t *testing.T) {
+	url := Murl + "update"
+	userJson := `{"Email":"sldfjsdlfeusdlfjsdlfj", "Password":"343223423423"}`
+
+	resp := doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("email type validation dont work")
+	}
+
+	userJson = `{"Role":"admin"}`
+
+	resp = doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("require validation dont work")
+	}
+
+	userJson = fmt.Sprintf("%s%d%s", `{"ID":`, Uidnew, `,"Role":"admin"}`)
+
+	fmt.Printf("%s\n", userJson)
+
+	resp = doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	return
+}
+
+//"/api/users/delete", App.Protect(srvDelete, []string{"admin"})).Methods("POST")
+func TestDelete(t *testing.T) {
+	url := Murl + "delete"
+	userJson := `{"ID":"0"}`
+
+	resp := doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u := readUserBody(resp, t)
+
+	if len(u.Errors) == 0 {
+		t.Fatal("wrong id validation dont work")
+	}
+
+	userJson = fmt.Sprintf("%s%d%s", `{"ID":`, Uid, `}`)
+
+	resp = doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	userJson = fmt.Sprintf("%s%d%s", `{"ID":`, Uidnew, `}`)
+
+	resp = doRequest(url, "POST", userJson, AdminToken)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Success expected: %d", resp.StatusCode)
+	}
+
+	u = readUserBody(resp, t)
+
+	if len(u.Errors) != 0 {
+		t.Fatal(u.Errors)
+	}
+
+	return
+}
