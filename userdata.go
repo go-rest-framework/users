32a72ab3d10c82943c85561c1e67237f685c5516
Oleksiy Hryshchenko <greeschenko@gmail.com>
2022-05-13 21:37:47 +0300

    up

--- userdata.go
+++ userdata.go
@@ -14,7 +14,7 @@ type UserData struct {
 	Data   User            `json:"data"`
 }
 
-func (u UserData) Read(r *http.Response) {
+func (u *UserData) Read(r *http.Response) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
